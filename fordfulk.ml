791c9a8d451624e35c8c4fca84c328cc945c1a23
rmonier <romain.monier0609@gmail.com>
2022-01-08 17:42:05 +0100

    WIP resolve
    Co-authored-by: Morgan Pelloux <hennrick.pelloux@etud.insa-toulouse.fr>

--- fordfulk.ml
+++ fordfulk.ml
@@ -6,62 +6,90 @@ let init_graph (gr: int graph) =
 ;;
 
 let rec path (gr : int graph) src dest =
-    let rec parcours_out_arcs gr src arcs acu =
+    let rec parcours_out_arcs gr src arcs chemin visited =
         match arcs with
-        | [] -> acu
-        | (id, lbl)::rst -> if id=dest && lbl > 0 then acu@[src; id] 
-        else if not (List.mem id acu) && lbl > 0 then
-            (match parcours_out_arcs gr id (out_arcs gr id) acu with
-            | [] -> parcours_out_arcs gr src rst acu
-            | l -> acu@[src]@l
-        )
-        else if lbl <= 0 then parcours_out_arcs gr src rst acu
-        else []
+        | [] -> chemin
+        | (id, lbl)::rst ->
+            Printf.printf "id %d \n" id ;
+            List.iter (Printf.printf "chemin in loop: %d\n") chemin;
+            if id=dest && lbl > 0 then
+                (chemin@[src])@[id]
+            else if not (List.mem id visited) && lbl > 0 then (
+                match parcours_out_arcs gr id (out_arcs gr id) chemin (visited@[id]) with
+                | [] -> parcours_out_arcs gr src rst chemin (visited@[id])
+                | l -> (chemin@[src])@l
+            ) else if lbl > 0 then (
+                parcours_out_arcs gr src rst chemin visited
+            ) else
+                parcours_out_arcs gr src rst chemin visited
     in
-    parcours_out_arcs gr src (out_arcs gr src) []
+    parcours_out_arcs gr src (out_arcs gr src) [] []
 ;;
+
+
+(*let rec path (gr: int graph) src dest =
+	let rec parcours_out_arcs gr src arcs visited =
+		match arcs with
+		| [] -> []
+		| (id, lbl)::rst ->
+		if id=dest && lbl > 0 then
+			[src; id]
+		else if lbl > 0 then
+			if not (List.mem id visited) then
+				match parcours_out_arcs gr id (out_arcs gr id) (visited@[id]) with
+				| [] -> parcours_out_arcs gr src rst visited
+				| l -> src::l
+			else
+				parcours_out_arcs gr src rst visited
+		else if lbl <= 0 then
+			parcours_out_arcs gr src rst visited
+		else
+			[]
+	in
+	parcours_out_arcs gr src (out_arcs gr src) []
+;;
+*)
 
 let rec find_augmentation (gr:int graph) path =
     let rec aux gr path acu =
         match path with
-        | hd::sec::rst -> (match find_arc gr hd sec with
-                            | Some x -> if acu == -1 || x < acu then aux gr (sec::rst) x
-                            else aux gr (sec::rst) acu
-                            | _ -> aux gr (sec::rst) acu)
+        | hd::sec::rst -> (
+            match find_arc gr hd sec with
+            | Some x ->
+                if acu == -1 || x < acu then
+                    aux gr (sec::rst) x
+                else
+                    aux gr (sec::rst) acu
+            | _ -> aux gr (sec::rst) acu
+        )
         | _ -> acu
     in
     aux gr path (-1)
 ;;
 
-(*let rec residual_graph c_gr path augm =
-    let rec forward gr path x =
-        match path with
-        | hd::sec::rst -> residual_graph (add_arc gr hd sec (x)) (sec::rst) augm
-        | _ -> gr
-    in
-    let rec backward gr path x =
-        match List.rev path with
-        | hd::sec::rst -> residual_graph (add_arc gr hd sec (-x)) (sec::rst) augm
-        | _ -> gr
-    in
-    backward (forward c_gr path augm) path augm
-;;*)
-
+(* on parcours les nodes dans path, et on applique la différence des arcs du graphe de capacité et du graphe de flot sur les arcs *)
 let residual_graph c_gr f_gr path =
+    (* on met à jour les arcs du graphe avec les capacités résiduelles (augmentation) *)
     let rec forward c f path =
         match path with
-        | hd::sec::rst -> (match find_arc f hd sec with
-                            | Some x -> forward (add_arc c hd sec (-x)) f (sec::rst)
-                            | _ -> c)
+        | hd::sec::rst -> (
+            match find_arc f hd sec with
+            | Some x -> forward (add_arc c hd sec (-x)) f (sec::rst)
+            | _ -> c
+        )
         | _ -> c        
     in
+    (* on met à jour les arcs du graphe avec les capacités résiduelles dans l'autre sens (diminution) *)
     let rec backward c f path =
         match path with
-        | hd::sec::rst -> (match find_arc f hd sec with
-                            | Some x -> backward (add_arc c sec hd x) f (sec::rst)
-                            | _ -> c)
+        | hd::sec::rst -> (
+            match find_arc f hd sec with
+            | Some x -> backward (add_arc c sec hd x) f (sec::rst)
+            | _ -> c
+        )
         | _ -> c
     in
+    let () = Printf.printf "residual_graph called\n" in
     backward (forward c_gr f_gr path) f_gr path
 ;;
 
@@ -69,11 +97,16 @@ let empty_flow_graph (c_gr:int graph) =
     gmap c_gr (fun x -> 0)
 ;;
 
+(* ajoute les valeurs d'augmention et diminution au graphe de flow *)
 let rec update_flow f_gr path augm =
+    (*let () = List.iter (Printf.printf "path: %d\n") path in*)
     match path with
-    | hd::sec::rst -> if find_arc f_gr hd sec != None 
-    then update_flow (add_arc f_gr hd sec augm) (sec::rst) augm
-    else update_flow (add_arc f_gr sec hd (-augm)) (sec::rst) augm
+    | hd::sec::rst -> (
+        if find_arc f_gr hd sec <> None then
+            update_flow (add_arc f_gr hd sec augm) (sec::rst) augm
+        else
+            update_flow (add_arc f_gr sec hd (-augm)) (sec::rst) augm
+    )
     | _ -> f_gr
 ;;
 
@@ -89,30 +122,14 @@ steps:
 
 let resolve_ff c_gr src snk =
     let rec aux gr fl src snk =
-        let pathOfArcs = path gr src snk in
-        match pathOfArcs with
-        | [] -> fl
-        | _ -> aux (residual_graph gr (update_flow fl pathOfArcs (find_augmentation gr pathOfArcs)) pathOfArcs) (update_flow fl pathOfArcs (find_augmentation gr pathOfArcs)) src snk
+        let pathOfArcs = path gr src snk in (
+            let () = List.iter (Printf.printf "path:%d\n") pathOfArcs in
+            match pathOfArcs with
+            | [] -> fl (* si plus de path on a fini, on renvoit le nouveau graphe *)
+            | _ -> (* sinon on soustrait le minimum à chaque label du path obtenu pour créer le graphe de sortie *)
+                let fl_gr = update_flow fl pathOfArcs (find_augmentation gr pathOfArcs) in
+                aux (residual_graph gr fl_gr pathOfArcs) fl_gr src snk
+        )
     in
     aux c_gr (empty_flow_graph c_gr) src snk
 ;;
-
-(* FIXME: with functions above *)
-(*
-let resolve_ff gr_in src snk =
-    let rec resolve_rec gr_in src snk =
-        let p = path gr_in src snk in
-        match p with
-        | [] -> gr_in (* si plus de path on a fini, on renvoit le nouveau graphe *)
-        | _ -> (* sinon on soustrait 1 à chaque label du path obtenu pour créer le graphe de sortie *)
-            let gr_out = gr_in in
-                e_iter gr_out (fun (e_in, e_out, lbl) ->
-                    if List.mem e_in p && List.mem e_out (* FIXME: devrait etre le suivant direct *) && lbl > 0 then
-                        add_arc gr_out e_in e_out (-1)
-                    else
-                        gr_out
-                )
-    in
-    resolve_rec gr_in src snk 
-;;
-*)
